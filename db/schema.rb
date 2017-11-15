f60d50b5161022dff6c17ec5979f297adbdf841c
Jonathan <jon.dandois@gmail.com>
2017-11-15 16:09:01 -0500

    base user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115195109) do
+ActiveRecord::Schema.define(version: 20171115203630) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20171115195109) do
     t.datetime "updated_at", null: false
     t.string "author_name"
     t.datetime "published_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
